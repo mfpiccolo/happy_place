8835b5dc3b7a85f971ee32562a9d0722d1717bbe
Mike Piccolo <mfpiccolo@gmail.com>
2014-10-31 16:56:35 -0700

    Fix syntax error

--- lib/happy_place/railtie.rb
+++ lib/happy_place/railtie.rb
@@ -1,10 +1,11 @@
 module HappyPlace
   class Railtie < Rails::Railtie
     initializer "your_gem_name.action_controller" do
-    ActiveSupport.on_load(:action_controller) do
-      puts "Extending #{self} with YourGemsModuleName::Controller"
-      # ActionController::Base gets a method that allows controllers to include the new behavior
-      include HappyPlace::Controller # ActiveSupport::Concern
+      ActiveSupport.on_load(:action_controller) do
+        puts "Extending #{self} with YourGemsModuleName::Controller"
+        # ActionController::Base gets a method that allows controllers to include the new behavior
+        include HappyPlace::Controller # ActiveSupport::Concern
+      end
     end
   end
 end
