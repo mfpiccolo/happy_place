37d99bf47eb2b1f25bfc585e316aa813d428bf82
Mike Piccolo <mfpiccolo@gmail.com>
2014-12-21 22:03:29 -0800

    Update railtie

--- lib/happy_place/railtie.rb
+++ lib/happy_place/railtie.rb
@@ -4,7 +4,7 @@ module HappyPlace
   class Railtie < Rails::Railtie
     initializer "your_gem_name.action_controller" do
       ActiveSupport.on_load(:action_controller) do
-        puts "Extending #{self} with YourGemsModuleName::Controller"
+        puts "Extending #{self} with HappyPlace::Controller"
         # ActionController::Base gets a method that allows controllers to include the new behavior
         include HappyPlace::Controller # ActiveSupport::Concern
       end
